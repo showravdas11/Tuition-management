d910ec65cd0d0802bef1c80242ce6540c113d752
showravdas11 <showravdas11@gmail.com>
2024-03-30 17:53:26 +0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/tution_management/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.tution_management
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:tution_management/Screens/OnboardScreen/Onboard.dart';
+import 'package:tution_management/Screens/SplashScreen/SplashScreen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: OnboardScreen(),
+    );
+  }
+}
